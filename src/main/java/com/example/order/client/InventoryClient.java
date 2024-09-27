dcc044ce62ce5e6649d1c1367b32c56349f90ed2
Darren Buller <stash-taco-remake@duck.com>
2024-09-27 15:04:02 +0100

    Initial Kafka

--- src/main/java/com/example/order/client/InventoryClient.java
+++ src/main/java/com/example/order/client/InventoryClient.java
@@ -1,19 +1,12 @@
 package com.example.order.client;
 
-import groovy.util.logging.Slf4j;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.service.annotation.GetExchange;
 
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-@Slf4j
 public interface InventoryClient {
-
-    Logger log = LoggerFactory.getLogger(InventoryClient.class);
 
     @GetExchange("/api/inventory")
     @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
@@ -21,7 +14,6 @@ public interface InventoryClient {
     boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
 
     default boolean fallbackMethod(String code, Integer quantity, Throwable throwable) {
-        log.info("Cannot get inventory for skucode {}, failure reason: {}", code, throwable.getMessage());
         return false;
     }
 }
--- src/main/java/com/example/order/service/OrderService.java
+++ src/main/java/com/example/order/service/OrderService.java
@@ -6,24 +6,37 @@ import org.springframework.stereotype.Service;
 
 import com.example.order.client.InventoryClient;
 import com.example.order.dto.OrderRequest;
+import com.example.order.event.OrderPlacedEvent;
 import com.example.order.model.Order;
 import com.example.order.repository.OrderRepository;
 
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
+@Slf4j
 public class OrderService {
     private final OrderRepository orderRepository;
     private final InventoryClient inventoryClient;
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
 
     public void placeOrder(OrderRequest orderRequest) {
         boolean inStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
         if (inStock) {
             var order = mapToOrder(orderRequest);
             orderRepository.save(order);
+
+            // Send notification email
+            OrderPlacedEvent orderPlacedEvent = new OrderPlacedEvent(order.getOrderNumber(),
+                    orderRequest.userDetails().email());
+            log.info("Start - Sending OrderPlacedEvent {} to Kafka topic order-placed", orderPlacedEvent);
+            kafkaTemplate.send("order-placed", orderPlacedEvent);
+            log.info("End - Sending OrderPlacedEvent {} to Kafka topic order-placed", orderPlacedEvent);
+
         } else {
             throw new RuntimeException("Product with Skucode " + orderRequest.skuCode() + "is not in stock");
         }
