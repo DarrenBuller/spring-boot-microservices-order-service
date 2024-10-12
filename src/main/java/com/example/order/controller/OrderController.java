c1f687a9416813e734fdb4548b0af3b700a1ad72
Darren Buller <stash-taco-remake@duck.com>
2024-10-12 21:50:56 +0100

    Add grafana reporting

--- src/main/java/com/example/order/controller/OrderController.java
+++ src/main/java/com/example/order/controller/OrderController.java
@@ -1,15 +1,19 @@
 package com.example.order.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import com.example.order.dto.OrderRequest;
 import com.example.order.service.OrderService;
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("/api/order")
 @RequiredArgsConstructor
+@Slf4j
 public class OrderController {
 
     private final OrderService orderService;
@@ -19,5 +23,10 @@ public class OrderController {
     public String placeOrder(@RequestBody OrderRequest orderRequest) {
         orderService.placeOrder(orderRequest);
         return "Order Placed Successfully";
+    }
+
+    public CompletableFuture<String> fallbackMethod(OrderRequest orderRequest, RuntimeException runtimeException) {
+        log.info("failed to create order");
+        return CompletableFuture.supplyAsync(() -> "Oops! Something went wrong, please order after some time!");
     }
 }
--- src/main/java/com/example/order/repository/OrderRepository.java
+++ src/main/java/com/example/order/repository/OrderRepository.java
@@ -1,8 +1,10 @@
 package com.example.order.repository;
 
+import io.micrometer.observation.annotation.Observed;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.order.model.Order;
 
+@Observed
 public interface OrderRepository extends JpaRepository<Order, Long> {
 }
