135e517c882f51e938d7f2f433c39d569a08ff82
Darren Buller <stash-taco-remake@duck.com>
2024-10-23 17:54:20 +0100

    add missed file and renames
    # Conflicts:
    #	src/test/java/com/example/order/OrderServiceApplicationTests.java

--- src/main/java/com/example/order/OrderServiceApplication.java
+++ src/main/java/com/example/order/OrderServiceApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OrderServicesApplication {
+public class OrderServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OrderServicesApplication.class, args);
+		SpringApplication.run(OrderServiceApplication.class, args);
 	}
 }
